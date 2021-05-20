81f6dff0ff6cb928e3d28338c5e028732ccc4d24
Arthur V Fain <arthurvfain@gmail.com>
2021-05-19 23:31:13 -0400

    commit tree 54ec838282deb24a53b772f694007f0a37f596b5

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(arr, name)
+{
+    return arr.filter(apple => apple.toUpperCase() === name.toUpperCase());
+}
+function fuzzyMatch(arr, name)
+{
+    return arr.filter(apple => [...apple][0]=== [...name][0]);
+}
+function matchName(arr,dName)
+{
+    return arr.filter(apple => apple.name.toUpperCase() === dName.toUpperCase());
+}
